87b511858595afdad0e3578ff3cb6e03c310ee58
sifu <2587107273@qq.com>
2018-03-16 22:07:24 +0800

    登陆逻辑优化

--- src/router/index.js
+++ src/router/index.js
@@ -25,18 +25,25 @@ let router = new Router({
  * 路由前置钩子
  */
 router.beforeEach((to, from, next) => {
-  //如果此路由需要验证
-  if (to.meta.auth) {
-    //如果当前角色有权限（权限信息加密后保存在session中，所以获取权限在sessionStorage中取）
-    if (to.meta.role.indexOf(sessionStorage.role) >= 0) {
-      next()
+  //如果用户未登陆且不是跳转登陆页
+  if(sessionStorage.role === undefined && to.path !== '/'){
+    //跳转登陆页
+    next('/')
+  }else{
+    //如果此路由需要验证
+    if (to.meta.auth) {
+      //如果当前角色有权限（权限信息加密后保存在session中，所以获取权限在sessionStorage中取）
+      if (to.meta.role.indexOf(sessionStorage.role) >= 0) {
+        next()
+      } else {
+        //无权限跳转路由到'/403'
+        next('/403');
+      }
     } else {
-      //无权限跳转路由到'/403'
-      next('/403');
+      next();
     }
-  } else {
-    next();
   }
+
 })
 
 export default router;
